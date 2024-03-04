455b96316d94b98713002f5f4258fe92d9f62b35
Pedro Contreras Avendano <pedro.contreras.avendano@gmail.com>
2024-03-03 20:22:47 -0500

    Add BASE_DIR to settings for templates

--- lukasDjango/settings.py
+++ lukasDjango/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'lukasDjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates' # This is where we tell Django to look for our templates
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
