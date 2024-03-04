197b29a9a4c1104e617d100d6679b96fe7850e1e
Pedro Contreras Avendano <pedro.contreras.avendano@gmail.com>
2024-03-04 00:49:26 -0500

    Add api urls

--- lukasDjango/urls.py
+++ lukasDjango/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('webscrapper.urls'))
+    path('', include('webscrapper.urls')),
+    path('api/', include('webscrapper.api.urls')),
 ]
