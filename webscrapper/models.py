e988a0a869fd2afbeb67680c4599af2603ffd5ca
Pedro Contreras Avendano <pedro.contreras.avendano@gmail.com>
2024-03-03 23:46:25 -0500

    Add devices model

--- webscrapper/models.py
+++ webscrapper/models.py
@@ -2,3 +2,15 @@ from django.db import models
 
 # Create your models here.
 # This is where we configure our database
+
+    
+class Devices(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    price = models.FloatField()
+    image = models.CharField(max_length=500)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
