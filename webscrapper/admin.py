c438b704fab672af8ca84e19e21b23d2621dc97c
Pedro Contreras Avendano <pedro.contreras.avendano@gmail.com>
2024-03-04 00:24:40 -0500

    Add new migrations

--- webscrapper/admin.py
+++ webscrapper/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Devices
-admin.site.register(Devices)
+from .models import Device, websitesToScrappe, Review, DevicePrices
+admin.site.register(Device)
+admin.site.register(websitesToScrappe)
+admin.site.register(Review)
+admin.site.register(DevicePrices)
--- webscrapper/models.py
+++ webscrapper/models.py
@@ -1,16 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 # This is where we configure our database
 
-    
-class Devices(models.Model):
+class websitesToScrappe(models.Model):
+    url = models.CharField(max_length=500)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.url
+class Device(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
     price = models.FloatField()
     image = models.CharField(max_length=500)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
-    
+
     def __str__(self):
         return self.name
+
+class Review(models.Model):
+    device = models.ForeignKey(Device, on_delete=models.CASCADE, related_name='reviews')
+    name = models.CharField(max_length=100)
+    review = models.TextField()
+    rating = models.FloatField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+class DevicePrices(models.Model):
+    device = models.ForeignKey(Device, on_delete=models.CASCADE, related_name='prices')
+    price = models.FloatField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.device.name
