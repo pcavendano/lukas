8e577304346c3f6ab142e701fc32734da03a2da1
Pedro Contreras Avendano <pedro.contreras.avendano@gmail.com>
2024-03-10 19:54:02 -0400

    Successfully fetching data from api and saving it to db

--- webscrapper/scrappers/bell_scrapper.py
+++ webscrapper/scrappers/bell_scrapper.py
@@ -1,7 +1,14 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+import django
+django.setup()
 import requests
 import json
+from webscrapper.models import Manufacturer
+from webscrapper.models import Model
+from webscrapper.models import CategoryItem
 
-def scrape_website(url):
+def scrape_and_save(url):
     # Function to scrape a website and return the JSON response
     try:
         # Send a GET request to the URL
@@ -10,7 +17,41 @@ def scrape_website(url):
         if response.status_code == 200:
             # Convert the response content to JSON
             json_data = response.json()
-            return json_data
+            print(json_data)
+            for model_data in json_data['models']:
+                manufacturer_data = model_data['manufacturer']
+                category_item_data = model_data['category_item']
+
+                # Save Manufacturer
+                manufacturer, _ = Manufacturer.objects.get_or_create(
+                    manufacturer_id=manufacturer_data['manufacturer_id'],
+                    manufacturer_code=manufacturer_data['manufacturer_code'],
+                    manufacturer_name=manufacturer_data['manufacturer_name']
+                )
+
+                # Save CategoryItem
+                category_item, _ = CategoryItem.objects.get_or_create(
+                    item_id=category_item_data['item_id'],
+                    item_order=category_item_data['item_order'],
+                    item_name=category_item_data['item_name']
+                )
+
+                # Save Model
+                model, created = Model.objects.get_or_create(
+                    manufacturer=manufacturer,
+                    model_id=model_data['model_id'],
+                    model_code=model_data['model_code'],
+                    model_name=model_data['model_name'],
+                    model_title=model_data['model_title'],
+                    category_item=category_item
+                )
+                
+                # Optionally print a message indicating whether the model was created or not
+                if created:
+                    print(f"Model {model.model_name} created successfully.")
+                else:
+                    print(f"Model {model.model_name} already exists.")
+
         else:
             print(f"Failed to fetch URL: {url}. Status code: {response.status_code}")
             return {"error": f"Failed to fetch URL: {url}. Status code: {response.status_code}"}
@@ -21,5 +62,4 @@ def scrape_website(url):
 # Example usage
 if __name__ == "__main__":
     # Replace 'example.com/api/data' with the URL that returns JSON
-    scraped_data = scrape_website("https://ws1-bell.sbeglobalcare.com/gc-ws-connect-1.9/rest/gcWsConnect/findCatalogModels?session_id=8a331dc7-2cca-4cbe-8458-f04e48927e3c&category_code=TRADEIN&manufacturer_code=AP&cache=true")
-    print(scraped_data)
+    scrape_and_save("https://ws1-bell.sbeglobalcare.com/gc-ws-connect-1.9/rest/gcWsConnect/findCatalogModels?session_id=8a331dc7-2cca-4cbe-8458-f04e48927e3c&category_code=TRADEIN&manufacturer_code=AP&cache=true")
