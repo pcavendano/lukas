80906268a4ed16560645a9f2de6fc1344495c43c
Pedro Contreras Avendano <pedro.contreras.avendano@gmail.com>
2024-03-11 20:56:41 -0400

    Update serializer to return last price

--- webscrapper/api/serializers.py
+++ webscrapper/api/serializers.py
@@ -1,14 +1,26 @@
-from rest_framework.serializers import ModelSerializer
-from webscrapper.models import Model, Manufacturer
+from rest_framework import serializers
+from webscrapper.models import Model, Manufacturer, ModelPrice
 
-class ManufacturerSerializer(ModelSerializer):
+class ManufacturerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Manufacturer
         fields = '__all__'
 
-class ModelsSerializer(ModelSerializer):
-    manufacturer = ManufacturerSerializer()  # Include the nested manufacturer field
+class ModelPriceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ModelPrice
+        fields = ('price', 'created')  # Assuming you want to include price and creation date only
+
+class ModelsSerializer(serializers.ModelSerializer):
+    manufacturer = ManufacturerSerializer()
+    last_price = serializers.SerializerMethodField()
+
+    def get_last_price(self, obj):
+        last_price_instance = obj.prices.order_by('-created').first()
+        if last_price_instance:
+            return last_price_instance.price
+        return None
+
     class Meta:
         model = Model
         fields = '__all__'
-
